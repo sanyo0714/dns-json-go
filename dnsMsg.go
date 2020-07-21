63ed182de650f04dde2f0ff55261fa0ddf53a4d4
sanyo <sanyo0714@163.com>
2020-07-21 11:18:15 +0800

    init

--- dnsMsg.go
+++ dnsMsg.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"github.com/miekg/dns"
 	"github.com/sanyo0714/dns-json-go/jsondns"
+	"log"
+	"net"
+	"strconv"
+	"strings"
 )
 
 // DNSMsg is contains dns.Msg
@@ -13,12 +17,162 @@ type DNSMsg struct {
 
 // DNS2JSON is a function dns message to json
 func (msg *DNSMsg) DNS2JSON() ([]byte, error) {
-	jsonMsg := new(jsondns.JSONMsg)
 
-	return json.Marshal(jsonMsg)
+	resp := new(jsondns.JSONMsg)
+	resp.Status = uint32(msg.Rcode)
+	resp.TC = msg.Truncated
+	resp.RD = msg.RecursionDesired
+	resp.RA = msg.RecursionAvailable
+	resp.AD = msg.AuthenticatedData
+	resp.CD = msg.CheckingDisabled
+
+	resp.Question = make([]jsondns.Question, 0, len(msg.Question))
+	for _, question := range msg.Question {
+		jsonQuestion := jsondns.Question{
+			Name: question.Name,
+			Type: question.Qtype,
+		}
+		resp.Question = append(resp.Question, jsonQuestion)
+	}
+
+	resp.Answer = make([]jsondns.JSONRR, 0, len(msg.Answer))
+	for _, rr := range msg.Answer {
+		jsonAnswer := &jsondns.JSONRR{}
+		jsonAnswer.MarshalRR(rr)
+		resp.Answer = append(resp.Answer, *jsonAnswer)
+	}
+
+	resp.Authority = make([]jsondns.JSONRR, 0, len(msg.Ns))
+	for _, rr := range msg.Ns {
+		jsonAuthority := &jsondns.JSONRR{}
+		jsonAuthority.MarshalRR(rr)
+		resp.Authority = append(resp.Authority, *jsonAuthority)
+	}
+
+	resp.Additional = make([]jsondns.JSONRR, 0, len(msg.Extra))
+	for _, rr := range msg.Extra {
+		jsonAdditional := &jsondns.JSONRR{}
+		jsonAdditional.MarshalRR(rr)
+		header := rr.Header()
+		if header.Rrtype == dns.TypeOPT {
+			opt := rr.(*dns.OPT)
+			resp.Status = ((opt.Hdr.Ttl & 0xff000000) >> 20) | (resp.Status & 0xff)
+			for _, option := range opt.Option {
+				if option.Option() == dns.EDNS0SUBNET {
+					edns0 := option.(*dns.EDNS0_SUBNET)
+					clientAddress := edns0.Address
+					if clientAddress == nil {
+						clientAddress = net.IP{0, 0, 0, 0}
+					} else if ipv4 := clientAddress.To4(); ipv4 != nil {
+						clientAddress = ipv4
+					}
+					resp.ECS = clientAddress.String() + "/" + strconv.FormatUint(uint64(edns0.SourceScope), 10)
+				}
+			}
+			continue
+		}
+		resp.Additional = append(resp.Additional, *jsonAdditional)
+	}
+
+	return json.Marshal(resp)
 }
 
 // JSON2DNS is a function json to dns message
-func (msg *DNSMsg) JSON2DNS(jsonMsg *jsondns.JSONMsg) {
+func (msg *DNSMsg) JSON2DNS(jsonMsg *jsondns.JSONMsg, udpSize uint16, ednsClientNetmask uint8) (reply *dns.Msg, err error) {
 
+	reply = msg.Copy()
+	reply.Truncated = jsonMsg.TC
+	reply.AuthenticatedData = jsonMsg.AD
+	reply.CheckingDisabled = jsonMsg.CD
+	reply.Rcode = dns.RcodeServerFailure
+
+	reply.Answer = make([]dns.RR, 0, len(jsonMsg.Answer))
+	for _, rr := range jsonMsg.Answer {
+		dnsRR, err := rr.UnmarshalRR()
+		if err != nil {
+			log.Println(err)
+		} else {
+			reply.Answer = append(reply.Answer, dnsRR)
+		}
+	}
+
+	reply.Ns = make([]dns.RR, 0, len(jsonMsg.Authority))
+	for _, rr := range jsonMsg.Authority {
+		dnsRR, err := rr.UnmarshalRR()
+		if err != nil {
+			log.Println(err)
+		} else {
+			reply.Ns = append(reply.Ns, dnsRR)
+		}
+	}
+
+	reply.Extra = make([]dns.RR, 0, len(jsonMsg.Additional)+1)
+	opt := new(dns.OPT)
+	opt.Hdr.Name = "."
+	opt.Hdr.Rrtype = dns.TypeOPT
+	if udpSize >= 512 {
+		opt.SetUDPSize(udpSize)
+	} else {
+		opt.SetUDPSize(512)
+	}
+	opt.SetDo(false)
+	ednsClientSubnet := jsonMsg.ECS
+	ednsClientFamily := uint16(1)
+	ednsClientAddress := net.IP(nil)
+	ednsClientScope := uint8(255)
+	if ednsClientSubnet != "" {
+		slash := strings.IndexByte(ednsClientSubnet, '/')
+		if slash < 0 {
+			err = jsondns.UnmarshalError{"Invalid client subnet"}
+			return
+		}
+		ednsClientAddress = net.ParseIP(ednsClientSubnet[:slash])
+		if ednsClientAddress == nil {
+			err = jsondns.UnmarshalError{"Invalid client subnet address"}
+			return
+		}
+		if ipv4 := ednsClientAddress.To4(); ipv4 != nil {
+			ednsClientAddress = ipv4
+		} else {
+			ednsClientFamily = 2
+		}
+		scope, err := strconv.ParseUint(ednsClientSubnet[slash+1:], 10, 8)
+		if err != nil {
+			err = jsondns.UnmarshalError{"Invalid client subnet address"}
+			return
+		}
+		ednsClientScope = uint8(scope)
+	}
+
+	if ednsClientAddress != nil {
+		if ednsClientNetmask == 255 {
+			if ednsClientFamily == 1 {
+				ednsClientNetmask = 24
+			} else {
+				ednsClientNetmask = 56
+			}
+		}
+		edns0Subnet := new(dns.EDNS0_SUBNET)
+		edns0Subnet.Code = dns.EDNS0SUBNET
+		edns0Subnet.Family = ednsClientFamily
+		edns0Subnet.SourceNetmask = ednsClientNetmask
+		edns0Subnet.SourceScope = ednsClientScope
+		edns0Subnet.Address = ednsClientAddress
+		opt.Option = append(opt.Option, edns0Subnet)
+	}
+	reply.Extra = append(reply.Extra, opt)
+	for _, rr := range jsonMsg.Additional {
+		dnsRR, err := rr.UnmarshalRR()
+		if err != nil {
+			log.Println(err)
+		} else {
+			reply.Extra = append(reply.Extra, dnsRR)
+		}
+	}
+
+	reply.Rcode = int(jsonMsg.Status & 0xf)
+	opt.Hdr.Ttl = (opt.Hdr.Ttl & 0x00ffffff) | ((jsonMsg.Status & 0xff0) << 20)
+	reply.Extra[0] = opt
+
+	return
 }
--- jsondns/dns.go
+++ jsondns/dns.go
@@ -1,5 +1,11 @@
 package jsondns
 
+import (
+	"fmt"
+	"github.com/miekg/dns"
+	"strings"
+)
+
 // JSONMsg is dns json struct
 type JSONMsg struct {
 	// Standard DNS response code (32 bit integer).
@@ -15,9 +21,9 @@ type JSONMsg struct {
 	// 	// Whether the client asked to disable DNSSEC
 	CD         bool       `json:"CD"`
 	Question   []Question `json:"Question"`
-	Answer     []RR       `json:"Answer,omitempty"`
-	Authority  []RR       `json:"Authority,omitempty"`
-	Additional []RR       `json:"Additional,omitempty"`
+	Answer     []JSONRR   `json:"Answer,omitempty"`
+	Authority  []JSONRR   `json:"Authority,omitempty"`
+	Additional []JSONRR   `json:"Additional,omitempty"`
 	ECS        string     `json:"edns_client_subnet,omitempty"`
 	Comment    string     `json:"Comment,omitempty"`
 }
@@ -26,18 +32,57 @@ type JSONMsg struct {
 type Question struct {
 	// FQDN with trailing dot
 	Name string `json:"name"`
-	// Standard DNS RR type
+	// Standard DNS JSONRR type
 	Type uint16 `json:"type"`
 }
 
-// RR is dns rr json struct
-type RR struct {
+// JSONRR is dns rr json struct
+type JSONRR struct {
 	// Always matches name in the Question section
 	Name string `json:"name"`
-	// Standard DNS RR type
+	// Standard DNS JSONRR type
 	Type uint16 `json:"type"`
 	// Record's time-to-live in seconds
 	TTL uint32 `json:"TTL"`
 	// Data for A - IP address as text
 	Data string `json:"data"`
+}
+
+// MarshalRR is marshal dns RR
+func (jsonRR *JSONRR) MarshalRR(rr dns.RR) {
+	rrHeader := rr.Header()
+	jsonRR.Name = rrHeader.Name
+	jsonRR.Type = rrHeader.Rrtype
+	jsonRR.TTL = rrHeader.Ttl
+	data := strings.SplitN(rr.String(), "\t", 5)
+	if len(data) >= 5 {
+		jsonRR.Data = data[4]
+	}
+}
+
+// UnmarshalRR is unmarshal  dns RR
+func (jsonRR *JSONRR) UnmarshalRR() (dnsRR dns.RR, err error) {
+	if strings.ContainsAny(jsonRR.Name, "\t\r\n \"();\\") {
+		return nil, UnmarshalError{fmt.Sprintf("Record name contains space: %q", jsonRR.Name)}
+	}
+	rrType, ok := dns.TypeToString[jsonRR.Type]
+	if !ok {
+		return nil, UnmarshalError{fmt.Sprintf("Unknown record type: %d", jsonRR.Type)}
+	}
+	if strings.ContainsAny(jsonRR.Data, "\r\n") {
+		return nil, UnmarshalError{fmt.Sprintf("Record data contains newline: %q", jsonRR.Data)}
+	}
+	zone := fmt.Sprintf("%s %d IN %s %s", jsonRR.Name, jsonRR.TTL, rrType, jsonRR.Data)
+	dnsRR, err = dns.NewRR(zone)
+	return
+}
+
+// UnmarshalError is unmarshal error message
+type UnmarshalError struct {
+	err string
+}
+
+// Error is ...
+func (e UnmarshalError) Error() string {
+	return "json-dns: " + e.err
 }
