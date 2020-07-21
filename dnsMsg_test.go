642cb3d9fa4360d1a92ac6cbb55a2f46c75c997a
sanyo <sanyo0714@163.com>
2020-07-21 11:47:23 +0800

    add test

--- dnsMsg_test.go
+++ dnsMsg_test.go
@@ -1,8 +1,11 @@
 package dnsjson
 
 import (
+	"encoding/json"
 	"github.com/miekg/dns"
+	"github.com/sanyo0714/dns-json-go/jsondns"
 	"log"
+	"net"
 	"testing"
 )
 
@@ -18,14 +21,67 @@ func TestDNSMsg_DNS2JSON(t *testing.T) {
 		A("www.a.shifen.com. 62 A 220.181.38.150"),
 	}
 
+	msg.SetQuestion("www.baidu.com", dns.TypeA)
+
+	edns0Subnet := new(dns.EDNS0_SUBNET)
+	edns0Subnet.Code = dns.EDNS0SUBNET
+	edns0Subnet.SourceScope = 0
+	edns0Subnet.Family = uint16(1)
+	edns0Subnet.SourceNetmask = uint8(24)
+	edns0Subnet.Address = net.ParseIP("10.10.10.10")
+
+	opt := new(dns.OPT)
+	opt.Hdr.Name = "."
+	opt.Hdr.Rrtype = dns.TypeOPT
+	opt.SetUDPSize(dns.DefaultMsgSize)
+	opt.Option = append(opt.Option, edns0Subnet)
+
+	msg.Extra = append(msg.Extra, opt)
+
 	dnsMsg := &DNSMsg{msg}
 
-	b, _ := dnsMsg.DNS2JSON()
-	log.Println(string(b))
+	json, _ := dnsMsg.DNS2JSON()
+	log.Println(string(json))
 
 }
 
 func TestDNSMsg_JSON2DNS(t *testing.T) {
+	msg := new(dns.Msg)
+	msg.MsgHdr.Response = true
+	msg.MsgHdr.RecursionAvailable = true
+	msg.MsgHdr.AuthenticatedData = false
+	msg.Answer = []dns.RR{
+		CNAME("www.baidu.com 1097 CNAME www.a.shifen.com"),
+		A("www.a.shifen.com. 62 A 220.181.38.149"),
+		A("www.a.shifen.com. 62 A 220.181.38.150"),
+	}
+
+	msg.SetQuestion("www.baidu.com", dns.TypeA)
+
+	edns0Subnet := new(dns.EDNS0_SUBNET)
+	edns0Subnet.Code = dns.EDNS0SUBNET
+	edns0Subnet.SourceScope = 0
+	edns0Subnet.Family = uint16(1)
+	edns0Subnet.SourceNetmask = uint8(24)
+	edns0Subnet.Address = net.ParseIP("10.10.10.10")
+
+	opt := new(dns.OPT)
+	opt.Hdr.Name = "."
+	opt.Hdr.Rrtype = dns.TypeOPT
+	opt.SetUDPSize(dns.DefaultMsgSize)
+	opt.Option = append(opt.Option, edns0Subnet)
+
+	msg.Extra = append(msg.Extra, opt)
+
+	dnsMsg := &DNSMsg{msg}
+
+	jsonByte, _ := dnsMsg.DNS2JSON()
+	log.Println(string(jsonByte))
+	// -------------------------------
+	jsonMsg := &jsondns.JSONMsg{}
+	json.Unmarshal(jsonByte, jsonMsg)
+
+	log.Println(jsonMsg)
 
 }
 
